e66e87eddc734db8a6154103a02c3574fd79761a
Daniil Shvalov <shvalovich@gmail.com>
2023-10-13 11:21:19 +0300

    feat(emacs): magit dotfiles

--- .config/emacs/config.el
+++ .config/emacs/config.el
@@ -2024,15 +2024,29 @@ Note that these rules can't contain anchored rules themselves."
 (use-package magit
   :custom
   (magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
+  :preface
+  (defun my-magit-process-environment (env)
+    "Add GIT_DIR and GIT_WORK_TREE to ENV when in a special directory.
+https://github.com/magit/magit/issues/460 (@cpitclaudel)."
+    (let ((default (file-name-as-directory (expand-file-name default-directory)))
+          (home (expand-file-name "~/")))
+      (when (string= default home)
+        (let ((gitdir (expand-file-name "~/dotfiles/")))
+          (push (format "GIT_WORK_TREE=%s" home) env)
+          (push (format "GIT_DIR=%s" gitdir) env))))
+    env)
   :general
   (nvmap
     :prefix "SPC g"
-    "g" 'magit-status)
+    "g" 'magit-status
+    "d" 'my-magit-dotfiles)
 
   (general-define-key
     :keymaps 'transient-base-map
     "<escape>" 'transient-quit-one)
   :config
+  (advice-add 'magit-process-environment
+              :filter-return #'my-magit-process-environment)
   (advice-add 'magit-status :around #'execute-at-project-root))
 
 
